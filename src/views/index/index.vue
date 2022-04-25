603ce46a8413ebb44bf0375352ad4a8d1fc97a13
mausen <1093890786@qq.com>
2022-04-25 23:55:28 +0800

    learning canvas drawImg function

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,15 +1,48 @@
 <script setup>
-    import { fromJSON } from 'postcss';
-    import { reactive } from 'vue';
-    import MsSwiperGoods from '@/components/ms-swiper-goods/ms-swiper-goods.vue';
-    import MsWrapper from '@/components/ms-wrapper/ms-wrapper.vue';
-    import MsBanner from '@/components/ms-banner/ms-banner.vue';
+import { fromJSON } from 'postcss';
+import { onMounted, reactive, ref } from 'vue';
+import MsSwiperGoods from '@/components/ms-swiper-goods/ms-swiper-goods.vue';
+import MsWrapper from '@/components/ms-wrapper/ms-wrapper.vue';
+import MsBanner from '@/components/ms-banner/ms-banner.vue';
+
+const drawCanvas = async () => {
+    const canvas = document.getElementById('canvas');
+    const image = await createImg();
+    const ctx = canvas.getContext("2d", {
+        antialias: true // 开启抗锯齿
+    });
+    // 初始化
+    ctx.clearRect(0, 0, 500, 500);
+    ctx.fillStyle = '#ffffff';
+    ctx.fillRect(0, 0, 500, 500)
+    ctx.save();
+    
+    //When there are five arguments, the drawImage function scales the image
+    const cutPicWidth = image.width / 2;
+    const cutPicHeight = image.height;
+    //Apart from element object ,the first four slices, the last four scales
+    ctx.drawImage(image, 0, 0, cutPicWidth, cutPicHeight, 0, 0, 400, 400);
+}
+const createImg = () => {
+    return new Promise((resolve) => {
+        const img = new Image();
+        img.src = '/src/static/images/animation-example.png';
+        img.setAttribute('crossorigin', 'anonymous')
+        img.onload = () => resolve(img)
+    })
+}
+onMounted(() => {
+    drawCanvas()
+})
 </script>
 
 <template>
     <div class="exhibition-area">
         <ms-wrapper></ms-wrapper>
-        <ms-banner></ms-banner>
+        <!-- <ms-banner></ms-banner> -->
+        <div class="canvas">
+            <canvas id="canvas" width="400" height="400"></canvas>
+        </div>
     </div>
     <main>
         <ms-swiper-goods></ms-swiper-goods>
@@ -18,4 +51,27 @@
 
 <style scoped lang='scss'>
 @import './index.scss';
+
+.canvas {
+    position: relative;
+
+    &::before {
+        content: 'Y';
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        width: 2px;
+        background-color: $primary-color;
+    }
+
+    &::after {
+        content: 'X';
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        height: 2px;
+    }
+}
 </style>
